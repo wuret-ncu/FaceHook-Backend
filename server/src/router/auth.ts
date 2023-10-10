dd59c748a619cc808a6c33c76418f66663ff4fe4
TINGYUNLI <rita891013@gmail.com>
2023-10-10 16:30:22 +0800

    add user route

---
+++ server/src/router/auth.ts
@@ -0,0 +1,67 @@
+import myDataSource from '../database/dbconfig'; 
+import express, { Request, Response } from 'express';
+import { Users } from '../entity/users';
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
+
+const router = express.Router();
+
+// 註冊使用者
+router.post('/register', async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userRepository = myDataSource.getRepository(Users); 
+
+    // 檢查email是否已被註冊
+    const existingUser = await userRepository.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: '該信箱已被註冊' });
+    }
+
+    // 創建新使用者
+    const newUser = userRepository.create({ username, email, password });
+    await userRepository.save(newUser);
+
+    return res.status(201).json({ msg: '註冊成功', newUser });
+  } catch (error) {
+    return res.status(500).json({ msg: '無法註冊使用者' , error });
+  }
+});
+
+// 登入
+router.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const userRepository = myDataSource.getRepository(Users);
+
+    // 尋找使用者
+    const user = await userRepository.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: '找不到使用者，請確認信箱正確' });
+    }
+
+    // 驗證密碼
+    const passwordMatch = await user.comparePassword(password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: '密碼錯誤' });
+    }
+    // 製作 json web token
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+    if (!process.env.PASSPORT_SECRET) {
+      throw new Error('PASSPORT_SECRET is not defined');
+    }
+    const token = jwt.sign(payload, process.env.PASSPORT_SECRET, { expiresIn: '1h' });
+
+    // return res.status(200).json({ msg: '登入成功', token:'JWT '+ token, user });
+    return res.status(200).json({token:'JWT '+ token });
+  } catch (error) {
+    return res.status(500).json({ msg: '無法登入', error });
+  }
+});
+
+export default router;
