bd7a802b88d7d07c03eb208a3dcb3f2cda771a87
eating31 <sun891201@gmail.com>
2023-10-10 15:53:40 +0800

    add test websocket

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+
 const app: Application = express();
 
 import myDataSource from "./database/dbconfig"
+
+const httpServer = require('http').createServer(app);
 
 // establish database connection
 myDataSource
@@ -16,6 +19,33 @@ myDataSource
 app.get('/',(req: Request, res: Response, next: NextFunction) => {
     res.send("Root route is working");
 });
-app.listen(3000,() => {
-    console.log("Server listening on port 3000");
+
+httpServer.listen(8888,() => {
+    console.log("Server listening on port 8888");
 });
+
+
+const io = require('socket.io')(httpServer);
+io.on("connection", function(socket: any){
+    console.log('user is connected')
+    // socket.on('getMessage', (message: string)  => {
+    //     console.log(message)
+    //     //回傳 message 給發送訊息的 Client
+    //     socket.emit('getMessage', message)
+    // })
+    // 檢查 socket 是否具有 .on 方法
+
+    console.log(typeof socket.on)
+    if (typeof socket.on === 'function') {
+        socket.on('getMessage', function(message: string){
+            console.log(message)
+            //回傳 message 給發送訊息的 Client
+            socket.emit('getMessage', message)
+        })
+    } else {
+        console.error('socket.on is not a function');
+    }
+})
+
+
+//app.listen(3000);
